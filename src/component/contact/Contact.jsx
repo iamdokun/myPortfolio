63be0f0ad2a68de3872cd6c0c6e3249f7c1c83f1
Abiodun <dokun.abraham@gmail.com>
2022-07-11 21:32:41 +0100

    feat: added testimonial and contact section

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./contact.scss"
 
 export default function Contact() {
+  const [message, setMessage] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
     <div className='contact' id='contact'>
-      
+      <div className="left">
+        <img src="assest/contact.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form action="" onSubmit={handleSubmit}>
+          <input type="text" placeholder='Email'/>
+          <textarea placeholder='Message'></textarea>
+          <button type='submit' onClick={() => setMessage(true)}>Send</button>
+        </form>
+        {message && <span>Thanks, I'll reply ASAP :)</span>}
+      </div>
     </div>
   )
 }
