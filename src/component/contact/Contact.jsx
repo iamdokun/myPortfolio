23f61ff25c7f243ae5d865139228f65b4fed8b69
Abiodun <dokun.abraham@gmail.com>
2022-07-12 22:59:42 +0100

    rearrange

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -1,25 +1,27 @@
-import React, { useState } from 'react'
-import "./contact.scss"
+import React, { useState } from "react";
+import "./contact.scss";
 
 export default function Contact() {
   const [message, setMessage] = useState(false);
   const handleSubmit = (e) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+  };
   return (
-    <div className='contact' id='contact'>
+    <div className="contact" id="contact">
       <div className="left">
         <img src="assest/contact.svg" alt="" />
       </div>
       <div className="right">
         <h2>Contact</h2>
         <form action="" onSubmit={handleSubmit}>
-          <input type="text" placeholder='Email'/>
-          <textarea placeholder='Message'></textarea>
-          <button type='submit' onClick={() => setMessage(true)}>Send</button>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit" onClick={() => setMessage(true)}>
+            Send
+          </button>
         </form>
         {message && <span>Thanks, I'll reply ASAP :)</span>}
       </div>
     </div>
-  )
+  );
 }
